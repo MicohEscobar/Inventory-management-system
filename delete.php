55d7013e1c3327a09ad4d891ce6d1727a603c33d
MicohEscobar <mayakadzanja@gmail.com>
2024-03-24 09:35:52 +0200

    added delete function

---
+++ delete.php
@@ -0,0 +1,15 @@
+<?php
+function deleteProduct($product_id){
+    global $conn;
+
+    $sql + "DELETE FROM products WHERE $product_id = $product_id";
+
+    if ($conn->query($sql)===TRUE) {
+        return "Product delete successfully";
+
+    }else {
+        return "Error deleting product: " . $conn->error;
+    }
+}
+
+?>
