981bcd88b2e85e966a74610afeafa2e5382b61c1
MicohEscobar <mayakadzanja@gmail.com>
2024-03-24 09:31:02 +0200

    modified update

--- update.php
+++ update.php
@@ -1,9 +1,9 @@
 <?php
-function updateProduct($product_id, $product_name, $category, $price, $price, $quantity, $cost){
+function updateProduct($product_id, $product_name, $category, $price, $quantity, $cost){
     global $conn;
 
     $sql = "UPDATE products
-            SET product_name = '$product_name', category = '$category', price = '$price', quantity = '$quantiy', cost = '$cost'
+            SET product_name = '$product_name', category = '$category', price = '$price', quantity = '$quantity', cost = '$cost'
             WHERE product_id = $product_id";
 
     if ($conn->query($sql) === TRUE) {
